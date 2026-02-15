160e856357c061c719d0e615b56b883c3a4c5605
IvanTeplykh <ivantieplih@gmail.com>
2026-02-15 11:56:30 +0000

    Deploying to gh-pages from @ IvanTeplykh/goit-js-hw-11@3871ef9f78f6d5f09031c04570b8919d5241e7ea 🚀

---
+++ index.js
@@ -0,0 +1,28 @@
+import{a as f,i as l,S as u}from"./assets/vendor-qbZl2uZI.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const a of o.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&i(a)}).observe(document,{childList:!0,subtree:!0});function n(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function i(e){if(e.ep)return;e.ep=!0;const o=n(e);fetch(e.href,o)}})();const m="54652097-fa33845d90f17562231f3e558";function y(r){return f({method:"get",url:"https://pixabay.com/api/",params:{key:m,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:40}}).then(function(t){return console.log(t.data.hits),t.data}).catch(function(t){l.error({title:"Error",message:"Sorry, there are no images matching your search query. Please try again!"})})}const h=u.default||u;let s;function p(r){const t=document.querySelector(".gallery"),n=r.map(i=>`<div class="photo-card">
+            <a href="${i.largeImageURL}">
+                <img src="${i.webformatURL}" alt="${i.tags}"/>
+            </a>
+            <div class="info">
+            <div class="info-item">
+                <p class="info-item">
+                    <b>Likes</b>${i.likes}
+                </p>
+                </div>
+                <div class="info-item">
+                <p class="info-item">
+                    <b>Views</b>${i.views}
+                </p>
+                </div>
+                <div class="info-item">
+                <p class="info-item">
+                    <b>Comments</b>${i.comments}
+                </p>
+                </div>
+                <div class="info-item">
+                <p class="info-item">
+                    <b>Downloads</b>${i.downloads}
+                </p>
+                </div>
+            </div>
+        </div>`).join("");t.innerHTML=n,s?s.refresh():s=new h(".gallery a",{captions:!0,captionsData:"alt",captionDelay:250,closeText:"×",navText:["←","→"]})}function g(){const r=document.querySelector(".gallery");r.innerHTML=""}function v(){document.querySelector(".loader").classList.remove("hidden")}function b(){document.querySelector(".loader").classList.add("hidden")}const d=document.querySelector(".form");document.querySelector(".gallery");document.querySelector(".loader");const S=document.querySelector(".js-search-query");let c="";d.addEventListener("submit",L);function L(r){r.preventDefault(),g(),v(),c=r.currentTarget.searchText.value.trim(),S.textContent=`Search query : "${c}"`,d.reset(),y(c).then(t=>{t.hits.length===0&&l.info({title:"Info",position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"}),p(t.hits)}).catch(t=>{l.error({title:"Error",position:"topRight",message:"Sorry, there are no images matching your search query. Please try again!"})}).finally(()=>{b()})}
+//# sourceMappingURL=index.js.map
